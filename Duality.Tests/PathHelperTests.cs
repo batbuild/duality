6df39273f5356c8868acf1748d3877af6d5ff868
Andrew O'Connor <andrew@batcatgames.com>
2014-05-08 17:13:54 +0100

    Improved performance of PathHelper.GetFileHash.

---
+++ Duality.Tests/PathHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+
+namespace Duality.Tests
+{
+	[TestFixture]
+	public class PathHelperTests
+	{
+		[Test]
+		public void GetFileHashReturnsSameHash()
+		{
+			var buffer = new Byte[10240000];
+			MathF.Rnd.NextBytes(buffer);
+			try
+			{
+				File.WriteAllText("Test.txt", BitConverter.ToString(buffer));
+				
+				Assert.AreEqual(PathHelper.GetFileHash("test.txt"), PathHelper.GetFileHash("test.txt"));
+			}
+			finally
+			{
+				if(File.Exists("test.txt"))
+					File.Delete("test.txt");
+			}
+		}
+	}
+}
