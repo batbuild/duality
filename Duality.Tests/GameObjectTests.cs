cb306e2f3072f173a4f59c309897cc817da8fe07
Andrea <roundcrisis@gmail.com>
2014-07-23 14:20:05 +0100

    Checking that it is not possible to set the gameObject as it's own parent

---
+++ Duality.Tests/GameObjectTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Duality.Resources;
+using NUnit.Framework;
+
+namespace Duality.Tests
+{
+	[TestFixture]
+	public class GameObjectTests
+	{
+		private GameObject _staticGameObject;
+
+		[SetUp]
+		public void Setup()
+		{
+			_staticGameObject = new GameObject("static");
+		}
+
+		[Test]
+		public void When_set_Parent_as_self_then_parent_still_null()
+		{
+			_staticGameObject.Parent = _staticGameObject;
+
+			Assert.IsNull(_staticGameObject.Parent);
+		}
+
+		[Test]
+		public void Parent_should_have_children_in_Childrens()
+		{
+			var parent = new GameObject("bla");
+			_staticGameObject.Parent = parent;
+
+			Assert.True(parent.Children.Count(x=> x == _staticGameObject) == 1);
+		}
+	}
+}
