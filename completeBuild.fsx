2f8eb060e8f021abd256ab570a4a19fd03af4008
Andrea <roundcrisis@gmail.com>
2014-07-25 17:10:56 +0100

    Clean up the build script by removing some comments and unnecessary variables

--- completeBuild.fsx
+++ completeBuild.fsx
@@ -13,10 +13,7 @@ let deployDir = @".\deploy\"
 let packagesDir = @".\packages"
 
 // Game directory this is where the assemblies will be copied to
-let gameDir = @"D:\honourbound\trunk\Game"
-
-// version info
-let version = "0.2"  // or retrieve from CI server
+let gameDir = @"D:\BatCat\honourbound\trunk\Game"
 
 // Targets
 Target "Clean" (fun _ ->
@@ -46,15 +43,6 @@ Target "NUnitTest" (fun _ ->
 let pluginsDir = buildDir+ "\Plugins"
 open System.IO
 Target "CopyToGame" (fun _ ->    
-(*    if Directory.Exists @"\DualityEditorPlugins\CamView\bin\Debug\" then CopyDir pluginsDir @".\DualityEditorPlugins\CamView\bin\Debug\CamView.editor.dll"  allFiles
-
-    CopyDir pluginsDir  @".\DualityEditorPlugins\EditorBase\bin\Debug\EditorBase.editor.dll"  allFiles
-    CopyDir pluginsDir @".\DualityEditorPlugins\HelpAdvisor\bin\Debug\HelpAdvisor.editor.dll"  allFiles
-    CopyDir pluginsDir @".\DualityEditorPlugins\LogView\bin\Debug\LogView.editor.dll" allFiles
-    CopyDir pluginsDir @".\DualityEditorPlugins\ObjectInspector\bin\Debug\ObjectInspector.editor.dll"  allFiles
-    CopyDir pluginsDir @".\DualityEditorPlugins\ProjectView\bin\Debug\ProjectView.editor.dll"  allFiles
-    CopyDir pluginsDir @".\DualityEditorPlugins\SceneView\bin\Debug\SceneView.editor.dll"  allFiles
-*)
     CopyDir gameDir @".\DualityEditor\bin\Debug" allFiles
 )
 
@@ -62,7 +50,7 @@ Target "Zip" (fun _ ->
     !+ (buildDir + "\**\*.*")
         -- "*.zip"
         |> Scan
-        |> Zip buildDir (deployDir + "Calculator." + version + ".zip")
+        |> Zip buildDir (deployDir + "Duality." + ".zip")
 )
 
 
