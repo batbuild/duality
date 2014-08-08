4f865cc8bd9e262a4e9490b092404c2570f8170e
Andrew O'Connor <bravesirandrew@gmail.com>
2014-08-08 22:13:28 +0100

    [PERFORMANCE] Parallelized GameObjectManager.ActiveObjects.
    This will probably end up being slower for low numbers of game
    objects, but in the thousands, it's quite a bit faster.

--- Duality/Utility/GameObjectManager.cs
+++ Duality/Utility/GameObjectManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Duality
 {
@@ -37,7 +38,7 @@ namespace Duality
 		{
 			get 
 			{
-				return this.allObj.Where(o => o.Active);
+				return this.allObj.AsParallel().Where(o => o.Active);
 			}
 		}
 		/// <summary>
