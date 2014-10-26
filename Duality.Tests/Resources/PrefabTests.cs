0351f3a38838125af7f501146a93cf9a76f532cc
Andrew O'Connor <bravesirandrew@gmail.com>
2014-10-26 09:49:18 +0000

    Bugfixes #FIX Fixed a bug in prefab link changes that made it impossible to have multiple changes with the same property name on the same object. For example, trying to set the ActiveSingle property would remove any existing changes to ActiveSingle regardless of what component it originally applied to.

---
+++ Duality.Tests/Resources/PrefabTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Duality.Components;
+using Duality.Components.Renderers;
+using Duality.Resources;
+using NUnit.Framework;
+
+namespace Duality.Tests.Resources
+{
+	[TestFixture]
+	public class PrefabTests
+	{
+		[Test]
+		public void PrefabChangesWithTheSamePropertyNameAreSaved()
+		{
+			var gameObj = this.CreateSimpleGameObject();
+			var prefab = new Prefab(gameObj);
+			gameObj.LinkToPrefab(prefab);
+
+			var transform = gameObj.GetComponent<Transform>();
+			var spriteRenderer = gameObj.GetComponent<SpriteRenderer>();
+			gameObj.PrefabLink.PushChange(transform, PropertyOf(() => transform.ActiveSingle), false);
+			gameObj.PrefabLink.PushChange(spriteRenderer, PropertyOf(() => spriteRenderer.ActiveSingle), false);
+
+			gameObj.PrefabLink.ApplyChanges();
+
+			Assert.IsFalse(gameObj.GetComponent<SpriteRenderer>().ActiveSingle);
+			Assert.IsFalse(gameObj.GetComponent<Transform>().ActiveSingle);
+		}
+
+		private GameObject CreateSimpleGameObject(GameObject parent = null)
+		{
+			var obj = new GameObject("SimpleObject", parent);
+			obj.AddComponent<Transform>();
+			obj.AddComponent<SpriteRenderer>();
+			return obj;
+		}
+
+		private static PropertyInfo PropertyOf<T>(Expression<Func<T>> expression)
+		{
+			var body = (MemberExpression)expression.Body;
+			return (PropertyInfo)body.Member;
+		}
+	}
+}
