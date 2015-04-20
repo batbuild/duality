d644d9ee376d988f6ec88ee6a803c7415f836b50
Andrea <roundcrisis@gmail.com>
2015-04-20 19:09:49 +0100

    Adding a duality launcher for android

---
+++ DualityLauncher/MainActivity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+
+namespace DualityLauncher.Android
+{
+	// the ConfigurationChanges flags set here keep the EGL context
+	// from being destroyed whenever the device is rotated or the
+	// keyboard is shown (highly recommended for all GL apps)
+	[Activity (Label = "DualityLauncher.Android",
+		#if __ANDROID_11__
+		HardwareAccelerated=false,
+		#endif
+		ConfigurationChanges = ConfigChanges.Orientation | 
+		ConfigChanges.KeyboardHidden | 
+		ConfigChanges.ScreenSize |
+		ConfigChanges.Orientation,
+		MainLauncher = true,
+		Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		ProgramAndroid view;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+
+			try
+			{
+				base.OnCreate (bundle);
+
+				// Create our OpenGL view, and display it
+				view = new ProgramAndroid (this);
+				SetContentView (view);
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine("there was a problem {0}", exception.StackTrace);
+			}
+		}
+
+		protected override void OnPause ()
+		{
+			// never forget to do this!
+			base.OnPause ();
+			view.Pause ();
+		}
+
+		protected override void OnResume ()
+		{
+			// never forget to do this!
+			base.OnResume ();
+			view.Resume ();
+		}
+	}
+}
+
+
---
+++ DualityLauncher/ProgramAndroid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES20;
+using OpenTK.Platform;
+
+using Android.Views;
+using Android.Content;
+using Android.Util;
+
+namespace DualityLauncher.Android
+{
+	class ProgramAndroid : OpenTK.Android.AndroidGameView
+	{
+		public ProgramAndroid (Context context) : base (context)
+		{
+		}
+
+		// This gets called when the drawing surface is ready
+		protected override void OnLoad (EventArgs e)
+		{
+			base.OnLoad (e);
+
+			// Run the render loop
+			Run ();
+		}
+
+
+		Random r = new Random ();
+		// This gets called on each frame render
+		protected override void OnRenderFrame (FrameEventArgs e)
+		{
+			// you only need to call this if you have delegates
+			// registered that you want to have called
+			base.OnRenderFrame (e);
+
+			GL.ClearColor ((float)r.NextDouble (), 0f, 255f, 0f);
+			GL.Clear (ClearBufferMask.ColorBufferBit);
+
+			SwapBuffers ();
+		}
+
+	}
+}
+
