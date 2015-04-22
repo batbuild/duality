1b918834838c5829a5d4fc3b5991731d1db5a67f
Andrea <roundcrisis@gmail.com>
2015-04-22 17:58:55 +0100

    Adding launcher functionality that doesn't depend on Duality (nearly nothing)

--- DualityLauncher/ProgramAndroid.cs
+++ DualityLauncher/ProgramAndroid.cs
@@ -11,10 +11,17 @@ using Android.Content;
 using Android.Util;
 using System.Diagnostics;
 
+
 namespace DualityLauncher.Android
 {
 	public class ProgramAndroid : AndroidGameView
 	{
+		private static bool _isDebugging = false;
+		private static bool _isProfiling = false;
+		private static bool _isRunFromEditor = false;
+		private Stopwatch _frameLimiterWatch = new Stopwatch();
+		Random r = new Random();
+
 		public ProgramAndroid (Context context) : base (context)
 		{
 		}
@@ -23,19 +30,61 @@ namespace DualityLauncher.Android
 		protected override void OnLoad (EventArgs e)
 		{
 			base.OnLoad (e);
-
-			// Run the render loop
-		
+/* DEBT: waiting for Duality x 3
+ */			
+			_isDebugging = System.Diagnostics.Debugger.IsAttached ;// || args.Contains(DualityApp.CmdArgDebug);
+//			isRunFromEditor = args.Contains(DualityApp.CmdArgEditor);
+//			isProfiling = args.Contains(DualityApp.CmdArgProfiling);
+			if (_isDebugging || _isRunFromEditor) ShowConsole();
 		}
 
+		private static bool hasConsole = false;
+		public static void ShowConsole()
+		{
+			/* DEBT: Not sure we need this console
+			 */			
+			if (hasConsole) return;
+		//	NativeMethods.AllocConsole();
+			hasConsole = true;
+		}
 
 		protected override void OnUpdateFrame(FrameEventArgs e)
 		{
 			base.OnUpdateFrame(e);
+
+			if (!_isDebugging && !_isProfiling) // Don't limit frame rate when debugging or profiling.
+			{
+				//// Assure we'll at least wait 16 ms until updating again.
+				//if (this.frameLimiterWatch.IsRunning)
+				//{
+				//    while (this.frameLimiterWatch.Elapsed.TotalSeconds < 0.016d) 
+				//    {
+				//        // Go to sleep if we'd have to wait too long
+				//        if (this.frameLimiterWatch.Elapsed.TotalSeconds < 0.01d)
+				//            System.Threading.Thread.Sleep(1);
+				//    }
+				//}
+
+				// Give the processor a rest if we have the time, don't use 100% CPU even without VSync
+				/* DEBT: waiting for Duality time is defined in duality, not sure what to do about VSync,
+				 * is it available on android?
+ */			
+//				if (_frameLimiterWatch.IsRunning && this.VSync == VSyncMode.Off)
+//				{
+//					while (_frameLimiterWatch.Elapsed.TotalMilliseconds < Time.MsPFMult)
+//					{
+//						// Enough leftover time? Risk a millisecond sleep.
+//						if (_frameLimiterWatch.Elapsed.TotalMilliseconds < Time.MsPFMult * 0.75f)
+//							System.Threading.Thread.Sleep(1);
+//					}
+//				}
+				_frameLimiterWatch.Restart();
+			}
+
 		}
 
-		Random r = new Random ();
-		// This gets called on each frame render
+		
+
 		protected override void OnRenderFrame (FrameEventArgs e)
 		{
 			// you only need to call this if you have delegates
