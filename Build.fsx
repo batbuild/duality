d369818cb686a55c87b92d170a13481d6ce5fd71
Andrea <roundcrisis@gmail.com>
2015-06-11 20:59:08 +0100

    Updating android build script

--- Build.fsx
+++ Build.fsx
@@ -4,66 +4,80 @@
 open Fake
 open Fake.AssemblyInfoFile
 
-RestorePackages()
-
 // Directories
 let buildDir  = @".\build\"
-let testDir   = @".\test\"
-let deployDir = @".\deploy\"
-let packagesDir = @".\packages"
+let packagesDir = @".\deploy\"
 
-// Game directory this is where the assemblies will be copied to
-let gameDir = @"D:\BatCat\honourbound\trunk\Game"
+let authors = ["Andrea Magnorsky"; "Andrew O'Connor"; "Dean Ellis";]
+
+// project name and description
+let projectName = "Duality.Android"
+let projectDescription = "Duality with Android runtime"
+let projectSummary = projectDescription // TODO: write a summary
+
+let version = "0.1."+ buildVersion  
+
 
 // Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; testDir; deployDir]
+    CleanDirs [buildDir; packagesDir] 
 )
 
-Target "CompileApp" (fun _ ->
-     !! "Duality.sln"      
-      |> MSBuildDebug buildDir "Build"
-      |> Log "AppBuild-Output: "
+Target "SetVersions" (fun _ ->
+    CreateCSharpAssemblyInfo "./Properties/AssemblyInfo.cs"
+        [Attribute.Title projectName
+         Attribute.Description projectDescription
+         Attribute.Product projectSummary
+         Attribute.Version version
+         Attribute.FileVersion version]
 )
 
-Target "CompileTest" (fun _ ->
-    !! @"*Test.csproj"
-      |> MSBuildDebug testDir "Build"
-      |> Log "TestBuild-Output: "
+Target "RestorePackages" (fun _ ->
+    !! "./**/packages.config"
+        |> Seq.iter (RestorePackage (fun p ->
+            { p with Sources = ["https://www.myget.org/F/6416d9912a7c4d46bc983870fb440d25/"]}))
 )
 
-Target "NUnitTest" (fun _ ->
-    !! (testDir + @"\NUnit.Test.*.dll")
-      |> NUnit (fun p ->
-                 {p with
-                   DisableShadowCopy = true;
-                   OutputFile = testDir + @"TestResults.xml"})
+Target "BuildUnsafe" (fun _ ->          
+    let buildMode = getBuildParamOrDefault "buildMode" "Release"
+    let setParams defaults =
+        { defaults with
+            Verbosity = Some(Normal)            
+            Properties =
+                [            
+                    "Configuration", buildMode                    
+                    "AllowUnsafeBlocks", "True"
+                ]
+        }
+    build setParams "./Duality.android.sln"    
+    |> DoNothing  
 )
 
-let pluginsDir = buildDir+ "\Plugins"
-open System.IO
-Target "CopyToGame" (fun _ ->    
-    CopyDir gameDir @".\DualityEditor\bin\Debug" allFiles
+Target "AndroidPack" (fun _ ->         
+    NuGet (fun p -> 
+        {p with 
+            Authors = authors
+            Project = projectName
+            Description = projectDescription      
+            Version = if isLocalBuild then "0.1-loc" else "0.1."+ buildVersion
+            AccessKey = getBuildParamOrDefault "nugetkey" ""
+            Publish = hasBuildParam "nugetkey"
+            PublishUrl = getBuildParamOrDefault "nugetUrl" ""            
+            WorkingDir = @".\"
+            OutputPath = packagesDir
+            Dependencies = [                            
+                            "Farseer.Duality.Android","0.1.7"
+                            ]
+        }) "NuGetPackageSpecs/duality.android.nuspec"
 )
-
-Target "Zip" (fun _ ->
-    !+ (buildDir + "\**\*.*")
-        -- "*.zip"
-        |> Scan
-        |> Zip buildDir (deployDir + "Duality." + ".zip")
-)
-
-
 
 // Dependencies
-"Clean"
-  ==> "CompileApp"
-(*  ==> "CompileTest"
-  ==> "FxCop"
-  ==> "NUnitTest"*)
-  ==> "CopyToGame"
-//  ==> "Zip"
+"Clean"    
+  ==> "SetVersions"
+  ==> "RestorePackages"
+  ==> "BuildUnsafe"    
+  ==> "AndroidPack"
+  
 
 
-// start build
-RunTargetOrDefault "CopyToGame"
+RunTargetOrDefault "AndroidPack"
