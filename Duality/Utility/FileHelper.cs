aceaf151941aeef246fc571fadb09de853b1bb40
Andrea <roundcrisis@gmail.com>
2015-07-11 17:08:13 +0100

    normalize paths on cache

--- Duality/Utility/FileHelper.cs
+++ Duality/Utility/FileHelper.cs
@@ -66,7 +66,7 @@ namespace Duality.Utility
 					while (reader.Peek() >= 0)
 					{
 						var filePath = reader.ReadLine();
-						filesInDataDir.Add(filePath);
+						filesInDataDir.Add(NormalizePath( filePath));
 					}
 				}
 				return true;
