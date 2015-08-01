2f6c04a07fa6d0046a1a13b73ce34dd0678cff67
Andrew O'Connor <bravesirandrew@gmail.com>
2015-08-01 18:28:05 +0100

    Bug fix #FIX: Pixmap was not returning the correct byte size for uncompressed images.

--- Duality/Resources/Pixmap.cs
+++ Duality/Resources/Pixmap.cs
@@ -156,7 +156,7 @@ namespace Duality.Resources
 			/// </summary>
 			public int ImageSize
 			{
-				get { return this.dataDisposable.RequestedSize; }
+				get { return this.dataDisposable.RequestedSize * 4; }
 			}
 			/// <summary>
 			/// [GET] The layers pixel data when DXT compression is being used
